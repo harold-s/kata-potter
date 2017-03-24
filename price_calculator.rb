6016b0d14ca16c41b900c5a71a610d69318421da
Harold Simpson <harold.simpson@storyful.com>
2017-03-24 15:25:33 +0000

    Refacto calculate to add discounts by sets

--- price_calculator.rb
+++ price_calculator.rb
@@ -14,17 +14,31 @@ class PriceCalculator
   end
 
   def calculate
-    books.count * BOOK_BASE_PRICE - discount
+    books.count * BOOK_BASE_PRICE - total_discount
   end
 
   private
 
-  def discount
-    discount_set(books.uniq.size)
+  def books_counts_by_nb
+    @books_counts_by_nb ||= {}.tap do |books_counts_by_nb|
+      books.each do |book_id|
+        books_counts_by_nb[book_id] ||= 0
+        books_counts_by_nb[book_id] += 1
+      end
+    end
   end
 
-  def discount_set(book_size)
-    discount_pc = DISCOUNTS[book_size] || 0
-    BOOK_BASE_PRICE * book_size * (discount_pc.to_f / 100)
+  def total_discount
+    set_nb_max = books_counts_by_nb.values.max || 0
+    (1..set_nb_max).map { |set_nb| discount_for_set_nb(set_nb) }.sum
+  end
+
+  def discount_for_set_nb(set_nb)
+    discount_book_nb(books_counts_by_nb.values.select { |v| v >= set_nb }.count)
+  end
+
+  def discount_book_nb(book_nb)
+    discount_pc = DISCOUNTS[book_nb] || 0
+    BOOK_BASE_PRICE * book_nb * (discount_pc.to_f / 100)
   end
 end
