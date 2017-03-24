6e798e9ab4c1b1638d6e3edb86bad298b0f339b1
Harold Simpson <harold.simpson@storyful.com>
2017-03-24 11:59:36 +0000

    PriceCalculator calculate change calculate price to include discount

--- price_calculator.rb
+++ price_calculator.rb
@@ -2,12 +2,24 @@
 class PriceCalculator
   attr_reader :books
   BOOK_BASE_PRICE = 8
+  DISCOUNTS = {
+    2 => 5,
+    3 => 10,
+    4 => 20,
+    5 => 25
+  }.freeze
 
   def initialize(books)
     @books = books
   end
 
   def calculate
-    books.count * BOOK_BASE_PRICE
+    books.count * BOOK_BASE_PRICE * (1 - (discount.to_f / 100))
+  end
+
+  private
+
+  def discount
+    DISCOUNTS[books.size] || 0
   end
 end
