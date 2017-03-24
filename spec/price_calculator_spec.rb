082e8c5a7b97edda581681f3aad9872c16d222b9
Harold Simpson <harold.simpson@storyful.com>
2017-03-24 11:09:11 +0000

    Spec PriceCalculator constructor

--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -2,4 +2,11 @@
 require_relative '../price_calculator'
 
 RSpec.describe PriceCalculator do
+  subject(:instance) { described_class.new(books) }
+  describe '#new' do
+    let(:books) { [0, 2] }
+    it 'sets the book array' do
+      expect(instance.books).to eq(books)
+    end
+  end
 end
